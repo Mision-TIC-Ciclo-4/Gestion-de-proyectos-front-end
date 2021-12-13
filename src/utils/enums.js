44773b2cbc6e4d04b796f5af77d6bfbde6196aec
Jhon Dorado <jh400@gmail.com>
2021-12-13 16:34:52 -0500

    se agrega editar y eliminar objetivos

---
+++ src/utils/enums.js
@@ -0,0 +1,31 @@
+const Enum_Rol = {
+  ADMINISTRADOR: 'Administrador',
+  ESTUDIANTE: 'Estudiante',
+  LIDER: 'Líder',
+};
+
+const Enum_EstadoUsuario = {
+  PENDIENTE: 'Pendiente',
+  AUTORIZADO: 'Autorizado',
+  NO_AUTORIZADO: 'No autorizado',
+};
+
+const Enum_EstadoProyecto = {
+  ACTIVO: 'Activo',
+  INACTIVO: 'Inactivo',
+};
+
+const Enum_TipoObjetivo = {
+  GENERAL: 'General',
+  ESPECIFICO: 'Específico',
+};
+
+const  Enum_FaseProyecto = {
+  INICIADO: 'Iniciado',
+  EN_DESARROLLO: 'En desarollo',
+  TERMINADO: 'Terminado',
+  NULO: 'Nulo',
+  
+};
+
+export { Enum_Rol, Enum_EstadoUsuario, Enum_EstadoProyecto, Enum_TipoObjetivo, Enum_FaseProyecto };
